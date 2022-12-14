066323b8a0bd6cfe197801c753afa72b8d61b2cc
zer0-x <zer0-x.7ty50@aleeas.com>
2022-12-14 20:26:11 +0300

    Create foxmarks-stamp file when building to detect cargo's outdir

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 use std::env;
 use std::io::Error;
+use std::fs::File;
+use std::path::Path;
 
 use clap_complete::{generate_to, Shell};
 
@@ -10,6 +12,12 @@ fn main() -> Result<(), Error> {
         None => return Ok(()),
         Some(outdir) => outdir,
     };
+
+    // HACK: Create a stamp file to detect where is the latest outdir.
+    let stamp_path = Path::new(&outdir).join("foxmarks-stamp");
+    if let Err(err) = File::create(&stamp_path) {
+        panic!("failed to write {}: {}", stamp_path.display(), err);
+    }
 
     let mut cmd = build_cli();
 
